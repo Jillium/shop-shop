4b9c5386a67b3acc9454f13875648d9a76813e76
Jillium <86424914+Jillium@users.noreply.github.com>
2022-02-14 12:06:29 -0700

    offline functionality

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,6 +6,7 @@ import { useQuery } from '@apollo/client';
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+import { idbPromise } from "../../utils/helpers";
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -16,9 +17,26 @@ function ProductList() {
 
   useEffect(() => {
     if (data) {
+      // let's store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
+      });
+      // but let's also take each product and save it to the IndexDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+
+      // add else if to check if 'loading' is undefined in 'useQuery()'
+
+    } else if (!loading) {
+      // since we're offline, get all of the data from the 'products' store 
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
       });
     }
   }, [data, dispatch]);
